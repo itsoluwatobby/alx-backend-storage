a54ed9e6a76e2a1c475a50e92771b0077c307ee2
itsoluwatobby <oluwasamuel050@gmail.com>
2024-03-28 04:19:37 +0100

    done

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -8,6 +8,43 @@ import functools
 from typing import Callable, Union, Optional
 
 
+def count_calls(method: Callable) -> Callable:
+    """
+    decorator that takes a single method Callable argument
+    and returns a Callable
+    """
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """
+        increments the count for that key every time the method
+        is called and returns the value returned by the original
+        method
+        """
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """
+    stores the history of inputs and outputs for a particular function
+    """
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """
+        saves the input and output of each function in redis
+        """
+        inputs_key = f"{method.__qualname__}:inputs"
+        outputs_key = f"{method.__qualname__}:outputs"
+
+        self._redis.rpush(inputs_key, str(args))
+        output = method(self, *args, **kwargs)
+        self._redis.rpush(outputs_key, str(output))
+        return output
+    return wrapper
+
+
 def replay(fn: Callable):
     """Display the history of calls of a particular function"""
     val = redis.Redis()
@@ -41,43 +78,6 @@ class Cache:
         """Instantiates an instance of the cache class"""
         self._redis = redis.Redis()
         self._redis.flushdb()
-
-    @staticmethod
-    def count_calls(method: Callable) -> Callable:
-        """
-        decorator that takes a single method Callable argument
-        and returns a Callable
-        """
-        @functools.wraps(method)
-        def wrapper(self, *args, **kwargs):
-            """
-            increments the count for that key every time the method
-            is called and returns the value returned by the original
-            method
-            """
-            key = method.__qualname__
-            self._redis.incr(key)
-            return method(self, *args, **kwargs)
-        return wrapper
-
-    @staticmethod
-    def call_history(method: Callable) -> Callable:
-        """
-        stores the history of inputs and outputs for a particular function
-        """
-        @functools.wraps(method)
-        def wrapper(self, *args, **kwargs):
-            """
-            saves the input and output of each function in redis
-            """
-            inputs_key = f"{method.__qualname__}:inputs"
-            outputs_key = f"{method.__qualname__}:outputs"
-
-            self._redis.rpush(inputs_key, str(args))
-            output = method(self, *args, **kwargs)
-            self._redis.rpush(outputs_key, str(output))
-            return output
-        return wrapper
 
     @call_history
     @count_calls
